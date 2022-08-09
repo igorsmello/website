5a76e617498eb38b272f66629f894e9ec911f2c0
Igor Mello <93830245+igorsmello@users.noreply.github.com>
2022-08-09 20:20:09 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////
 
-// Galeria de Imagens - modal
+// Galeria de Imagens - modal - design arts
 let img= document.querySelectorAll('.small_img');
 let modal = document.querySelector('.modal');
 let modalImg = document.querySelector('#modal_img');
@@ -17,11 +17,35 @@ for(let i = 0 ; i < img.length ; i++){
     
 }
 
-
 btClose.addEventListener('click', function(){
     modal.classList.toggle('modal_active');
 });
 
 /////////////////////////////////////////////////////////////////////////////
 
+// Animação de aparição dos elementos Home
+
+// Slider portfolio.html
+
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+setInterval(next, 5000);
+
+
+
+
+ 
 
